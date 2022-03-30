4a0b4e57daa122c05829b19cd23fadd022443a95
Aysan Esmradi <64140743+AysanEsmradi@users.noreply.github.com>
2022-03-30 23:51:42 +0800

    Update preparing_data.py

--- preparing_data.py
+++ preparing_data.py
@@ -13,6 +13,8 @@ def prepare_data():
 
     df = df.query('Language=="en"')
     df = df[['user_id', 'isbn', 'rating', 'book_title', 'Summary', 'Category', 'img_m']]
+    df.drop(index=df[df['Category'] == '9'].index, inplace=True)    #The 9 is null values in category
+    df.drop(index=df[df['Summary'] == '9'].index, inplace=True)    #The 9 is null values in Summary
     print(df.isna().sum())  # check null values
 
     item_df = df[['isbn', 'book_title', 'Summary', 'Category', 'img_m']].copy()
